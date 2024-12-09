d5bf1a4db6cedc0f2e3d81a398bb54e954ce993c
Danny <dannydotdev@gmail.com>
2024-12-09 10:10:17 +0100

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
+
+
+git init
+git add .
+git commit -m "DAN - initial config"
+git branch -M main
+git remote add origin https://github.com/1am-programmer/dao.git
+git push -u origin main
