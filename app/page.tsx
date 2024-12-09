66f7c7f39a790b31f9e9942f295fd9abd0e9b281
Danny <dannydotdev@gmail.com>
2024-12-09 17:34:16 +0100

    nextConfig

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,13 @@
 import About from "@/components/About";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import MobileNav from "@/components/MobileNav";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    // <div className="bg-web3-gradient text-gray-300">
     <div className=" ">
-      {/* <Header /> */}
       <MobileNav />
       <Hero />
       <About />
-      {/* <Footer /> */}
-      {/* </div> */}
     </div>
   );
 }
